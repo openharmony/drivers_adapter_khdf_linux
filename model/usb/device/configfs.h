4a58f72cb2599d9e419c61faf35baa1754c26f34
zianed <houxuanzhe@huawei.com>
2021-07-31 18:05:55 +0800

    add oat
    Signed-off-by: zianed <houxuanzhe@huawei.com>

--- model/usb/device/configfs.h
+++ model/usb/device/configfs.h
@@ -1,4 +1,21 @@
-/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * configfs.h
+ *
+ * usb configfs adapter of linux
+ *
+ * Copyright (c) 2020-2021 Huawei Device Co., Ltd.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #ifndef USB__GADGET__CONFIGFS__H
 #define USB__GADGET__CONFIGFS__H
 
