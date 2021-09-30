e95b2d86ea7eab3d15aa63e919b3d08a34ed3a37
yinshuqing <yinshuqing@huawei.com>
2021-09-30 11:25:00 +0800

    0929代码告警处理
    Signed-off-by: yinshuqing <yinshuqing@huawei.com>

--- platform/mipi_csi/mipi_csi_dev.c
+++ platform/mipi_csi/mipi_csi_dev.c
@@ -17,22 +17,13 @@
  */
 
 #include "mipi_csi_dev.h"
-#include <linux/cdev.h>
-#include <linux/device.h>
-#include <linux/fs.h>
-#include <linux/init.h>
-#include <linux/kdev_t.h>
 #include <linux/miscdevice.h>
-#include <linux/module.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/version.h>
-#include "hdf_base.h"
 #include "hdf_log.h"
 #include "mipi_csi_core.h"
-#include "osal_io.h"
 #include "osal_mem.h"
-#include "osal_mutex.h"
 #include "securec.h"
 
 #ifdef __cplusplus
