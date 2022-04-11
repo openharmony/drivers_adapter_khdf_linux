a75db91587692a3ba0f54b9ece0d572d3f20ac02
baoguotao <baoguotao@huawei.com>
2022-04-11 09:05:13 +0800

    fix: Optimization function deal_format nesting too deep
    Signed-off-by: baoguotao <baoguotao@huawei.com>

--- osal/src/osal_deal_log_format.c
+++ osal/src/osal_deal_log_format.c
@@ -20,10 +20,10 @@
 #include "securec.h"
 
 #define NUMBER 2
-static const char *g_property[NUMBER] = {"%{private}", "%{public}"};
-static size_t g_property_len[NUMBER] = {10, 9};
+static const char *g_property[NUMBER] = { "%{private}", "%{public}" };
+static size_t g_property_len[NUMBER] = { 10, 9 };
 
-/* remove "{private}" and "{public}" from fmt and copy to dest */
+/* remove "{private}" and "{public}" from fmt and copy the rest to dest */
 bool deal_format(const char *fmt, char *dest, size_t size)
 {
 	const char *ptr = fmt;
@@ -38,26 +38,27 @@ bool deal_format(const char *fmt, char *dest, size_t size)
 	}
 
 	while (ptr_cur != NULL && *ptr_cur != '\0') {
-		if (*ptr_cur == '%') {
-			for (i = 0; i < NUMBER; i++) {
-				if (strncmp(ptr_cur, g_property[i], g_property_len[i]) == 0) {
-					/* add 1 is for to copy char '%' */
-					ret = strncpy_s(&dest[index], size - index, ptr, ptr_cur - ptr + 1);
-					if (ret != EOK) {
-						printk("%s strncpy_s error %d", __func__, ret);
-						return false;
-					}
-					index += (ptr_cur - ptr + 1);
-					ptr = ptr_cur + g_property_len[i];
-					ptr_cur = ptr;
-					break;
-				}
-			}
-			if (i == NUMBER)
-				ptr_cur++;
-		} else {
+		if (*ptr_cur != '%') {
 			ptr_cur++;
+			continue;
 		}
+		for (i = 0; i < NUMBER; i++) {
+			if (strncmp(ptr_cur, g_property[i], g_property_len[i]) != 0)
+				continue;
+
+			/* add 1 is for to copy char '%' */
+			ret = strncpy_s(&dest[index], size - index, ptr, ptr_cur - ptr + 1);
+			if (ret != EOK) {
+				printk("%s strncpy_s error %d", __func__, ret);
+				return false;
+			}
+			index += (ptr_cur - ptr + 1);
+			ptr = ptr_cur + g_property_len[i];
+			ptr_cur = ptr;
+			break;
+		}
+		if (i == NUMBER)
+			ptr_cur++;
 	}
 	ret = strcat_s(&dest[index], size - index, ptr);
 	if (ret != EOK) {
